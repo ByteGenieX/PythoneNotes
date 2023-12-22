c51a3a208de992fe9fd5070c7922eb422790d792
Deepak Kumar Maurya <maurya9@gmail.com>
2023-12-23 00:16:33 +0530

    add if statement file

---
+++ cal.py
@@ -0,0 +1,6 @@
+print(5+6)
+num=34
+print(num)
+user_one_name="deepak" #snake case write base used on python
+UserOneName="Divyanshu"#camel caes write base used on java
+print (user_one_name+UserOneName)
---
+++ if_statement.py
@@ -0,0 +1,7 @@
+winn_number = 27
+user_input = input("input any number in between 1 to 100 =")
+user_input1 = int(user_input)
+if user_input1 == winn_number:
+    print("you winn")
+else:
+    print("you loss!")
